34fe59f3dddb0b9aa4b86f7b3595a61d4d6ebe1d
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2022-01-19 22:12:42 +0200

    setup front end events

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      text: 'Doctors Appointment',
+      day: 'Jan 19th at 9pm',
+      reminder: true,
+    },
+    {
+      id: '2',
+      text: 'Shopping',
+      day: 'Jan 20th at 9am',
+      reminder: false,
+    },
+    {
+      id: '3',
+      text: 'School',
+      day: 'Jan 21th at 8am',
+      reminder: true,
+    }
+  ]);
+
+  // DELETE TASK
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // TOGGLE REMINDER
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task)=>task.id===id?{...task,reminder:!task.reminder}:task))
+  }
+  return (  
+    <div className="container">
+      <Header title='Hello' />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
   );
 }
